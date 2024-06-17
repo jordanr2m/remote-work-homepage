cfb8ca833e76378abc5315e3820ec1ed26456bdf
Jordan J <jordanr2m@gmail.com>
2024-06-17 16:31:15 -0700

    Finished adding styles for Homepage.

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,4 +1,9 @@
 import "./Homepage.css"
+/* Import images */
+import databiz from "../../assets/client-databiz.svg";
+import audiophile from "../../assets/client-audiophile.svg";
+import meet from "../../assets/client-meet.svg";
+import maker from "../../assets/client-maker.svg";
 
 const Homepage = () => {
     return (
@@ -8,10 +13,13 @@ const Homepage = () => {
             <main className="content wrapper">
                 <h1>Make remote work</h1>
                 <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
-                <button>Learn More</button>
+                <button className="learn-more">Learn more</button>
 
                 <div className="endorsement-icons">
-                    
+                    <img src={databiz} alt="databiz"/>
+                    <img src={audiophile} alt="audiophile"/>
+                    <img src={meet} alt="meet"/>
+                    <img src={maker} alt="maker"/>
                 </div>
             </main>
         </section>
