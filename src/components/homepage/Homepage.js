3c14b696fac55c72cb82e784e4a049d280c6b859
Jordan J <jordanr2m@gmail.com>
2024-06-17 15:56:39 -0700

    Began adding content to homepage component.

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,11 +1,21 @@
 import "./Homepage.css"
 
 const Homepage = () => {
-  return (
-    <div>
-      <p>Homepage</p>
-    </div>
-  )
+    return (
+        <section className="homepage">
+            <div className="hero-img"></div>
+
+            <main className="content wrapper">
+                <h1>Make remote work</h1>
+                <p>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+                <button>Learn More</button>
+
+                <div className="endorsement-icons">
+                    
+                </div>
+            </main>
+        </section>
+    )
 }
 
 export default Homepage
